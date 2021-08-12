d0f36beeb358c7bc15e3e5d3f7a6e596a345c986
crogmi <chrisogami16@gmail.com>
2021-08-12 10:09:14 -0700

    Updated navbar styling for mobile devices

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavHashLink } from "react-router-hash-link";
 import ROUTES from "../../routes";
 import "./Navbar.css";
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    
+    const toggleNav = (event) => {
+        event.preventDefault();
+        setOpen(!open);
+    }
+    
     return (
         <nav className="navbar">
-            <ul className="nav-list">
-                <li className="nav-list-item">
+            <ul className={`nav-list ${open ? "open" : ""}`}>
+                <li className="nav-list-item" onClick={toggleNav}>
                     <NavHashLink className="nav-link" smooth to={ROUTES.HOME}>Home</NavHashLink>
                 </li>
-                <li className="nav-list-item">
+                <li className="nav-list-item" onClick={toggleNav}>
                     <NavHashLink className="nav-link" smooth to={ROUTES.ABOUT}>About</NavHashLink>
                 </li>
-                <li className="nav-list-item">
+                <li className="nav-list-item" onClick={toggleNav}>
                     <NavHashLink className="nav-link" smooth to={ROUTES.PORTFOLIO}>Portfolio</NavHashLink>
                 </li>
-                <li className="nav-list-item">
+                <li className="nav-list-item" onClick={toggleNav}>
                     <NavHashLink className="nav-link" smooth to={ROUTES.CONTACT}>Get in Touch</NavHashLink>
                 </li>
             </ul>
+            <div className="nav-hamburger" onClick={toggleNav}>
+                <div className="line" />
+                <div className="line" />
+                <div className="line" />
+            </div>
         </nav>
     );
 };
